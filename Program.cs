4a09a35446cba77c794e648bb5ec3f712b653b0d
OllieOD <odonoho@stpaulsschool.org.uk>
2022-10-07 11:17:10 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Assesment_07._10._22
+{
+    internal class Program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Enter a whole number");
+            int Number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a whole number");
+            int Number2 = int.Parse(Console.ReadLine());
+            int Temp1 = Number1;
+            int Temp2 = Number2;
+            while (Temp1 != Temp2)
+            {
+                if (Temp1 > Temp2)
+                {
+                    Temp1 = Temp1 - Temp2;
+                }
+                else
+                {
+                    Temp2 = Temp2 - Temp1;
+                }
+            }
+            int result = Temp1;
+            Console.WriteLine(result + " is GCF of" + Number1 + " and " + Number2);
+
+        }
+        // The While loop waws written using Temp1 and 2 so that the original values 
+        // would remain the same so they could be output at the end with Temp1
+    }
+}
---
+++ Question6.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Coding_Assesment_07._10._22
+{
+    internal class Question6
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a decimal number");
+            int deci = int.Parse(Console.ReadLine());
+            int temp = deci;
+            int length = 32;
+            int[] arr = new int[length];
+            int count = 0;
+            while (temp >= 0 && count < length)
+            {
+                if (temp % 2 == 1)
+                {
+                    arr[count] = 1;
+                    temp = temp / 2;
+                }
+                else
+                {
+                    arr[count] = 0;
+                    temp = temp / 2;
+                }
+                count++;
+            }
+            for (int i = length -1; i >= 0; i--)
+            {
+                Console.Write(arr[i]);
+            }
+            Console.Write("\n");
+            
+        }
+    }
+}
